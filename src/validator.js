cb00e4d6c156ab414fde89b751e30d953088dced
JenifferGenoves <jenifer.genoves03@gmail.com>
2020-04-12 05:29:14 -0400

    cambios ultima versión

--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,14 @@
-const validator = {
-  // ...
-};
+//import maskify from 'maskify';
 
-export default validator;
+const validator = {
+
+    maskify : function () {
+  
+        let texto = document.getElementById("tdc").value;
+        let mascara = "############"+ texto.substring(12);
+        //let mascara = maskify(texto);
+        console.log("Este es el numero de tarjeta enmascarada "+ mascara);
+        //document.write("Tu numero de tarjeta de credito termina en " + mascara);  
+        return mascara;
+    },
+    
