c6f3fe960e898cb69d7f72ffc1e8cdd8610641e7
natalieechenique <natalie.terapias@gmail.com>
2020-04-13 10:13:24 -0400

    cambios finales estilo y primera funcion validator

--- src/index.js
+++ src/index.js
@@ -1,195 +1,204 @@
-//  import validator from './validator.js';
-//  console.log(validator);
 
-
-
-//funcion generica. Se declara esta funcion como principal y después más abajo uno llena con el id que corresponde. Se pone -ocultarYMostrar- en este caso, y lo que va entre parentesis respeta el mismo orden de ocultarlo o mostrarlo, independiente dessl nombre que le pongamos, con ese id llamamos al elemento a ocultar o mostrar desde HTML. -getElementById llama al id, desde una lista de classes - classList- y da la opcion de agregar mas elementos ocultos con -.add- y remover con -remove--->
-function ocultarYMostrar(ocultarID, mostrarID) {
-  document.getElementById(ocultarID).classList.add("oculto");
-  document.getElementById(mostrarID).classList.remove("oculto");
-}
-
-function seleccionarIngresar() {
-  ocultarYMostrar("bienvenido", "entrarUsuario");
-}
-
-function seleccionarRegistrarse() {
-  ocultarYMostrar("bienvenido", "registroUsuario");
-}
-
-
-//-----------------------------------------------------//
-
-//INGRESO//
-
-function botonEnviarIngreso() {
-  ocultarYMostrar("entrarUsuario", "holaUsuario");
-}
-
-function botonCancelarIngreso() {
-  ocultarYMostrar("entrarUsuario","bienvenido" );
-}
-
-function botonSalirUsuario() {
-  ocultarYMostrar("holaUsuario","bienvenido" );
-}
-
-
-//-----------------------------------------------------//
-
-//REGISTRO//
-
-function botonIngresoTarj() {
-  ocultarYMostrar("registroUsuario", "tarjeta");
-}
-
-function botonCancelarRegistro() {
-  ocultarYMostrar("registroUsuario", "bienvenido");
-}
-
-//-----------------------------------------------------//
-
-//TARJETA//
-
-function botonValidarT() {
-  ocultarYMostrar("tarjeta", "holaUsuario");
-}
-
-function botonCancelValT() {
-  ocultarYMostrar("validarTarjetaC", "registroUsuario");
-}
-
-//-----------------------------------------------------/
-//ENVIAR FORMULARIO GENERAL, FIN//
-
-
-
-
-
-//FORMULARIO DE TARJETA DE CRÉDITO DINÁMICA//
-
-const tarjeta = document.querySelector('#tarjeta'),
-	  btnAbrirFormulario = document.querySelector('#btn-abrir-formulario'),
-	  formulario = document.querySelector('#formulario-tarjeta'),
-	  numeroTarjeta = document.querySelector('#tarjeta .numero'),
-	  nombreTarjeta = document.querySelector('#tarjeta .nombre'),
-	  logoMarca = document.querySelector('#logo-marca'),
-	  firma = document.querySelector('#tarjeta .firma p'),
-	  mesExpiracion = document.querySelector('#tarjeta .mes'),
-	  yearExpiracion = document.querySelector('#tarjeta .year');
-    ccv = document.querySelector('#tarjeta .ccv');
-    
-// * Volteamos la tarjeta para mostrar el frente.
-const mostrarFrente = () => {
-	if(tarjeta.classList.contains('active')){
-		tarjeta.classList.remove('active');
+ 
+  
+function validator(value) {
+	// acepta solo digitos
+	if (/[^0-9-\s]+/.test(value)) return false;
+  
+	//Luhn 
+	let validator = 0,
+	  bEven = false;
+	value = value.replace(/\D/g, '');
+  
+	for (var n = value.length - 1; n >= 0; n--) {
+	  var cDigit = value.charAt(n),
+		nDigit = parseInt(cDigit, 10);
+  
+	  if (bEven && (nDigit *= 2) > 9) nDigit -= 9;
+  
+	  validator += nDigit;
+	  bEven = !bEven;
 	}
-}
-
-// * Rotacion de la tarjeta
-tarjeta.addEventListener('click', () => {
-	tarjeta.classList.toggle('active');
-});
-
-// * Boton de abrir formulario
-btnAbrirFormulario.addEventListener('click', () => {
-	btnAbrirFormulario.classList.toggle('active');
-	formulario.classList.toggle('active');
-});
-
-// * Select del mes generado dinamicamente.
-for(let i = 1; i <= 12; i++){
-	let opcion = document.createElement('option');
-	opcion.value = i;
-	opcion.innerText = i;
-	formulario.selectMes.appendChild(opcion);
-}
-
-// * Select del año generado dinamicamente.
-const yearActual = new Date().getFullYear();
-for(let i = yearActual; i <= yearActual + 8; i++){
-	let opcion = document.createElement('option');
-	opcion.value = i;
-	opcion.innerText = i;
-	formulario.selectYear.appendChild(opcion);
-}
-
-// * Input numero de tarjeta
-formulario.inputNumero.addEventListener('keyup', (e) => {
-	let valorInput = e.target.value;
-
-	formulario.inputNumero.value = valorInput
-	// Eliminamos espacios en blanco
-	.replace(/\s/g, '')
-	// Eliminar las letras
-	.replace(/\D/g, '')
-	// Ponemos espacio cada cuatro numeros
-	.replace(/([0-9]{4})/g, '$1 ')
-	// Elimina el ultimo espaciado
-	.trim();
-
-	numeroTarjeta.textContent = valorInput;
-
-	if(valorInput == ''){
-		numeroTarjeta.textContent = '#### #### #### ####';
-
-		logoMarca.innerHTML = '';
+	if (value !== '') {
+	  return alert(validator % 10 == 0);
 	}
-
-	if(valorInput[0] == 4){
-		logoMarca.innerHTML = '';
-		const imagen = document.createElement('img');
-		imagen.src = 'img/logos/visa.png';
-		logoMarca.appendChild(imagen);
-	} else if(valorInput[0] == 5){
-		logoMarca.innerHTML = '';
-		const imagen = document.createElement('img');
-		imagen.src = 'img/logos/mastercard.png';
-		logoMarca.appendChild(imagen);
-	}
-
-	// Volteamos la tarjeta para que el usuario vea el frente.
-	mostrarFrente();
-});
-
-// * Input nombre de tarjeta
-formulario.inputNombre.addEventListener('keyup', (e) => {
-	let valorInput = e.target.value;
-
-	formulario.inputNombre.value = valorInput.replace(/[0-9]/g, '');
-	nombreTarjeta.textContent = valorInput;
-	firma.textContent = valorInput;
-
-	if(valorInput == ''){
-		nombreTarjeta.textContent = 'Jhon Doe';
-	}
-
-	mostrarFrente();
-});
-
-// * Select mes
-formulario.selectMes.addEventListener('change', (e) => {
-	mesExpiracion.textContent = e.target.value;
-	mostrarFrente();
-});
-
-// * Select Año
-formulario.selectYear.addEventListener('change', (e) => {
-	yearExpiracion.textContent = e.target.value.slice(2);
-	mostrarFrente();
-});
-
-// * CCV
-formulario.inputCCV.addEventListener('keyup', () => {
-	if(!tarjeta.classList.contains('active')){
-		tarjeta.classList.toggle('active');
-	}
-
-	formulario.inputCCV.value = formulario.inputCCV.value
-	// Eliminar los espacios
-	.replace(/\s/g, '')
-	// Eliminar las letras
-	.replace(/\D/g, '');
-
-	ccv.textContent = formulario.inputCCV.value;
-});
+	return alert('el campo tarjeta no puede estar vacío')
+  }
+  
+  
+  //DECL FUNCION GENERICA DOM
+  function ocultarYMostrar(ocultarID, mostrarID) {
+	document.getElementById(ocultarID).classList.add('oculto');
+	document.getElementById(mostrarID).classList.remove('oculto');
+  }
+  
+  //--------De bienvenida a formulario registro------------//
+  function seleccionarRegistro() {
+	ocultarYMostrar('bienvenido', 'registroUsuario');
+  }
+  
+  //----De registro a formulario validación tarjeta---//
+  function irFormularioTarjeta() {
+	ocultarYMostrar('registroUsuario', 'formulario-tarjeta');
+  }
+  
+  function cancelFormularioTarjeta() {
+	ocultarYMostrar('registroUsuario', 'bienvenido');
+  }
+  
+  //----De validacion tarjeta a OK, reintentar o cancelar, luego del PROMPT o alert correspondiente----//
+  function okTarjForm() {
+	ocultarYMostrar('formulario-tarjeta', 'bienvenido');
+  }
+  
+  function reintTarjForm() {
+	ocultarYMostrar('formulario-tarjeta', 'registroUsuario'); //acà deberìan limpiarse los campos//
+  }
+  
+  function cancelTarjForm() {
+	ocultarYMostrar('formulario-tarjeta', 'registroUsuario');
+  }
+  
+  function testvalidation() {
+	const num = document.getElementById('inputNumero').value;
+	validator(num);
+  }
+  
+  function testcancel() {
+	 ocultarYMostrar('formulario-tarjeta', 'registroUsuario');
+  }
+  
+  
+  
+  
+  
+  //5500 0000 0000 0004  is a valid number card for validation //
+  
+  
+  
+  
+  
+  
+  
+	//TARJETA INTERACTIVA-----//
+  
+	// const tarjeta = document.querySelector('#tarjeta');
+	// btnAbrirFormulario = document.querySelector('#btn-abrir-formulario');
+	// formulario = document.querySelector('#formulario-tarjeta');
+	// numeroTarjeta = document.querySelector('#tarjeta .numero');
+	// nombreTarjeta = document.querySelector('#tarjeta .nombre');
+	// logoMarca = document.querySelector('#logo-marca');
+	// firma = document.querySelector('#tarjeta .firma p');
+	// mesExpiracion = document.querySelector('#tarjeta .mes');
+	// yearExpiracion = document.querySelector('#tarjeta .year');
+	// ccv = document.querySelector('#tarjeta .ccv');
+  
+	//formulario tarjeta con funcion//
+  
+	// // * Input nombre de tarjeta
+	// formulario.inputNombre.addEventListener('keyup', (e) => {
+	//   let valorInput = e.target.value;
+  
+	//   formulario.inputNombre.value = valorInput.replace(/[0-9]/g, '');
+	//   nombreTarjeta.textContent = valorInput;
+	//   firma.textContent = valorInput;
+  
+	//   if (valorInput == '') {
+	//     nombreTarjeta.textContent = 'Juan Pérez';
+	//   }
+  
+	//   mostrarFrente();
+	// });
+  
+	// // * Select mes
+	// formulario.selectMes.addEventListener('change', (e) => {
+	//   mesExpiracion.textContent = e.target.value;
+	//   mostrarFrente();
+	// });
+  
+	// // * Select Año
+	// formulario.selectYear.addEventListener('change', (e) => {
+	//   yearExpiracion.textContent = e.target.value.slice(2);
+	//   mostrarFrente();
+	// });
+  
+	// // * CCV
+	// formulario.inputCCV.addEventListener('keyup', () => {
+	//   if (!tarjeta.classList.contains('active')) {
+	//     tarjeta.classList.toggle('active');
+	//   }
+  
+	//   formulario.inputCCV.value = formulario.inputCCV.value
+	//     // Eliminar los espacios
+	//     .replace(/\s/g, '')
+	//     // Eliminar las letras
+	//     .replace(/\D/g, '');
+  
+	//   ccv.textContent = formulario.inputCCV.value;
+	// });
+  
+  
+  
+  
+  
+  
+  
+  
+  
+	//TARJETA INTERACTIVA-----//
+  
+	// const tarjeta = document.querySelector('#tarjeta');
+	// btnAbrirFormulario = document.querySelector('#btn-abrir-formulario');
+	// formulario = document.querySelector('#formulario-tarjeta');
+	// numeroTarjeta = document.querySelector('#tarjeta .numero');
+	// nombreTarjeta = document.querySelector('#tarjeta .nombre');
+	// logoMarca = document.querySelector('#logo-marca');
+	// firma = document.querySelector('#tarjeta .firma p');
+	// mesExpiracion = document.querySelector('#tarjeta .mes');
+	// yearExpiracion = document.querySelector('#tarjeta .year');
+	// ccv = document.querySelector('#tarjeta .ccv');
+  
+	//formulario tarjeta con funcion//
+  
+	// // * Input nombre de tarjeta
+	// formulario.inputNombre.addEventListener('keyup', (e) => {
+	//   let valorInput = e.target.value;
+  
+	//   formulario.inputNombre.value = valorInput.replace(/[0-9]/g, '');
+	//   nombreTarjeta.textContent = valorInput;
+	//   firma.textContent = valorInput;
+  
+	//   if (valorInput == '') {
+	//     nombreTarjeta.textContent = 'Juan Pérez';
+	//   }
+  
+	//   mostrarFrente();
+	// });
+  
+	// // * Select mes
+	// formulario.selectMes.addEventListener('change', (e) => {
+	//   mesExpiracion.textContent = e.target.value;
+	//   mostrarFrente();
+	// });
+  
+	// // * Select Año
+	// formulario.selectYear.addEventListener('change', (e) => {
+	//   yearExpiracion.textContent = e.target.value.slice(2);
+	//   mostrarFrente();
+	// });
+  
+	// // * CCV
+	// formulario.inputCCV.addEventListener('keyup', () => {
+	//   if (!tarjeta.classList.contains('active')) {
+	//     tarjeta.classList.toggle('active');
+	//   }
+  
+	//   formulario.inputCCV.value = formulario.inputCCV.value
+	//     // Eliminar los espacios
+	//     .replace(/\s/g, '')
+	//     // Eliminar las letras
+	//     .replace(/\D/g, '');
+  
+	//   ccv.textContent = formulario.inputCCV.value;
+	// });
+  
--- src/validator.js
+++ src/validator.js
@@ -4,3 +4,109 @@ const validator = {stringNum
 };
 
 export default validator;
+
+
+
+
+
+
+const validator = {
+  //maskify 
+  const ensureOnlyNumbers = R.replace(/[^0-9]+/g, '');
+  const maskAllButLastFour = R.replace(/[0-9](?=([0-9]{4}))/g, '*');
+  const hashedCardNumber = R.compose(maskAllButLastFour, ensureOnlyNumbers);
+
+  const isValid = (value) => {
+    // acepta solo digitos
+    if (/[^0-9-\s]+/.test(value)) return false;
+
+    //Luhn 
+    let validator = 0,
+      bEven = false;
+    value = value.replace(/\D/g, '');
+
+    for (var n = value.length - 1; n >= 0; n--) {
+      var cDigit = value.charAt(n),
+        nDigit = parseInt(cDigit, 10);
+
+      if (bEven && (nDigit *= 2) > 9) nDigit -= 9;
+
+      validator += nDigit;
+      bEven = !bEven;
+    }
+    if (value !== '') {
+      return alert(validator % 10 == 0);
+    }
+    return alert('campo vacío');
+  }
+};
+
+export default validator;
+
+
+const hola = () => {
+  a = 'hola'
+  return a;
+}
+
+export default hola;
+
+
+
+
+
+
+
+
+//src/validator.js
+//Acá escribirás las funciones necesarias para que el usuario pueda verificar la tarjeta de crédito y ocultar los dígitos de su número de tarjeta. Esta función debe ser pura e independiente del DOM.
+
+//Para esto debes implementar el objeto validator, el cual ya se encuentra exportado en el boilerplate. Este objeto (validator) contiene dos métodos (isValid y maskify):
+
+//validator.isValid(creditCardNumber): creditCardNumber es un string con el número de tarjeta que se va a verificar. Esta función debe retornar un boolean dependiendo si es válida de acuerdo al algoritmo de Luhn.
+
+//validator.maskify(creditCardNumber): creditCardNumber es un string con el número de tarjeta y esta función debe retornar un string donde todos menos los últimos cuatro caracteres sean reemplazados por un numeral (#) o 🐱. Esta función deberá siempre mantener los últimos cuatro caracteres intactos, aún cuando el string sea de menor longitud.
+
+//Ejemplo de uso
+
+//maskify('4556364607935616') === '############5616'
+//maskify(     '64607935616') ===      '#######5616'
+//maskify(               '1') ===                '1'
+//maskify(               '')  ===                ''
+//src/index.js
+//Acá escribirás todo el código que tenga que ver con la //interacción del DOM (seleccionar, actualizar y manipular //elementos del DOM y eventos). Es decir, en este archivo //deberás invocar las funciones isValid y maskify según sea //necesario para actualizar el resultado en la pantalla (UI).
+
+//test/validator.spec.js
+//En este archivo tendrás que completar las pruebas unitarias de las funciones validator.isValid(creditCardNumber) y validator.maskify(creditCardNumber) implementadas en validator.js utilizando Jest. Tus pruebas unitarias deben dar un 70% en coverage (cobertura), statements (sentencias), functions (funciones) y lines (líneas); y un mínimo del 50% de branches (ramas).
+
+
+// importamos el objeto `validator`, que contiene las funciones `isValid` y `maskify`
+//import validator from '../src/validator';
+
+
+
+//describe('validator', () => {
+  //test('debería ser un objeto', () => {
+    //expect(typeof validator).toBe('object');
+  //});
+
+  //describe('validator.isValid', () => {
+   // test('debería ser una función', () => {
+     // expect(typeof validator.isValid).toBe('function');
+    //});
+
+    //test.skip('debería retornar true para "4083952015263"', () => {
+      // escribe aquí tu test
+    //});
+  //});
+
+ // describe('validator.maskify', () => {
+   // test('debería ser una función', () => {
+     // expect(typeof validator.maskify).toBe('function');
+    //});
+
+    //test.skip('Debería retornar "############5616" para //"4556364607935616"', () => {
+      // escribe aquí tu test
+   // });
+  //});
+//});
